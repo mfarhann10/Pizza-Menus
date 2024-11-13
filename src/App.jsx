6b782ede66999dad9b7a3bbb0e285e394bf3668d
mfarhann10 <mfarhann1009@gmail.com>
2024-11-14 02:19:42 +0700

    pizza display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,22 @@
-import { useEffect, useState } from "react";
 import "../public/css/styles.css";
 import Pizza from "./components/Pizza";
+import pizzaData from "./data/pizza";
 /* eslint-disable react/prop-types */
 function App() {
-  const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
-
-  async function getAdvice() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
-    setAdvice(data.slip.advice);
-    setCount((c) => c + 1);
-  }
-
-  useEffect(function () {
-    getAdvice();
-  }, []);
   return (
-    <div className="flex flex-col justify-center items-center space-y-2">
-      <h1 className="font-bold text-2xl">Learn React from scratch</h1>
-      <p className="font-semibold">{advice}</p>
-      <Message count={count} />
-      <button onClick={getAdvice} className="px-4 py-2 rounded-lg bg-cyan-200">
-        Get Advice
-      </button>
-      <Pizza />
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+      {pizzaData.map((pizza, index) => (
+        <Pizza
+          key={index}
+          id={index}
+          img={pizza.photoName}
+          name={pizza.name}
+          ingredients={pizza.ingredients}
+          price={pizza.price}
+          soldOut={pizza.soldOut}
+        />
+      ))}
     </div>
-  );
-}
-
-function Message(props) {
-  return (
-    <p className="font-semibold">
-      You have read {props.count} pieces of advices
-    </p>
   );
 }
 
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -1,5 +1,17 @@
-function Pizza() {
-  return <h2>Pizza</h2>;
+/* eslint-disable react/prop-types */
+function Pizza(props) {
+  return (
+    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white m-4 transform hover:scale-105 transition-transform duration-300 ease-in-out">
+      <img src={`/${props.img}`} alt={props.name} className="space-y-3" />
+
+      <div className="p-6">
+        <h1 className="font-bold text-xl text-gray-800 mb-2">{props.name}</h1>
+        <p className="text-gray-700 text-base mb-4">{props.ingredients}</p>
+        <p className="text-green-600 font-semibold text-lg">${props.price}</p>
+        <p className="font-bold">{props.soldOut ? "Ready" : "Sold Out"}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Pizza;
