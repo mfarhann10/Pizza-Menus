f9e8453fae328da230f6dfca3d0255457d82ce7e
mfarhann10 <mfarhann1009@gmail.com>
2024-11-15 02:16:53 +0700

    fix the components menu and pizza

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,15 +5,7 @@ function Menu() {
   return (
     <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
       {pizzaData.map((pizza, index) => (
-        <Pizza
-          key={index}
-          id={index}
-          photoName={pizza.photoName}
-          name={pizza.name}
-          ingredients={pizza.ingredients}
-          price={pizza.price}
-          soldOut={pizza.soldOut}
-        />
+        <Pizza key={index} id={index} pizzaObj={pizza} />
       ))}
     </main>
   );
--- src/components/Pizza.jsx
+++ src/components/Pizza.jsx
@@ -2,13 +2,27 @@
 function Pizza(props) {
   return (
     <div className="max-w-sm rounded-lg overflow-hidden shadow-lg bg-white m-4 transform hover:scale-105 transition-transform duration-300 ease-in-out">
-      <img src={`/${props.photoName}`} alt={props.name} className="space-y-3" />
+      <img
+        src={`/${props.pizzaObj.photoName}`}
+        alt={props.pizzaObj.name}
+        className="space-y-3"
+      />
 
       <div className="p-6">
-        <h1 className="font-bold text-xl text-gray-800 mb-2">{props.name}</h1>
-        <p className="text-gray-700 text-base mb-4">{props.ingredients}</p>
-        <p className="text-green-600 font-semibold text-lg">${props.price}</p>
-        <p className="font-bold">{props.soldOut ? "Ready" : "Sold Out"}</p>
+        <h1 className="font-bold text-xl text-gray-800 mb-2">
+          {props.pizzaObj.name}
+        </h1>
+        <p className="text-gray-700 text-base mb-4">
+          {props.pizzaObj.ingredients}
+        </p>
+        <p className="text-green-600 font-semibold text-lg">
+          ${props.pizzaObj.price}
+        </p>
+        {props.pizzaObj.soldOut ? (
+          <p className="font-bold">Ready</p>
+        ) : (
+          <p className="font-bold">Sold out</p>
+        )}
       </div>
     </div>
   );
